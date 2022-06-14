45d7f3d676d9ad575a6eb574e913c4ead5daae9f
Aya Al Rahman <yotayot29@gmail.com>
2022-06-14 15:29:38 +0300

    storage

--- routes/web.php
+++ routes/web.php
@@ -91,8 +91,8 @@ Route::view('/a', 'dashboard.crud.index');
 
 Route::get('aya',function(){
     set_time_limit(0);
-    Artisan::call('storage:link');
-    Artisan::call('migrate:fresh --seed');
+//    Artisan::call('storage:link');
+//    Artisan::call('migrate:fresh --seed');
     return 'success db_seed';
 });
 
