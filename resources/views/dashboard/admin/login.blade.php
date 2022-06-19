14d47a01d434409c67948198959bcca8d9d4cfab
Aya Al Rahman <yotayot29@gmail.com>
2022-06-19 20:14:46 +0300

    scripts

--- resources/views/dashboard/admin/login.blade.php
+++ resources/views/dashboard/admin/login.blade.php
@@ -6,7 +6,6 @@
     <title>Login to Admin Panel</title>
     <meta name="description" content="Login page example"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"/>
     <!--end::Fonts-->
     <!--begin::Page Custom Styles(used by this page)-->
     <link href="{{asset('assets/css/pages/login/login-2.css')}}" rel="stylesheet" type="text/css"/>
@@ -18,10 +17,7 @@
 
     <!--end::Layout Themes-->
     <link rel="shortcut icon" href="{{asset('assets/media/logos/favicon.ico')}}"/>
-{{--    {!! htmlScriptTagJsApi([--}}
-{{--           'action' => 'homepage',--}}
-{{--           'custom_validation' => 'myCustomValidation'--}}
-{{--       ]) !!}--}}
+
 
 </head>
 <!--end::Head-->
@@ -76,25 +72,8 @@
                                     <span class="text-danger">Current Password Error !</span>
                                 @endif
                             </div>
-
-                            <div class="form-group{{ $errors->has('g-recaptcha-response') ? ' has-error' : '' }}">
-                                <label class="col-md-4 control-label">Captcha</label>
-
-
-                                <div class="col-md-6">
-                                    {!! app('captcha')->display() !!}
-
-
-                                    @if ($errors->has('g-recaptcha-response'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <!--end::Form group-->
                             <!--begin::Action-->
+
                             <div class="text-center pt-2">
                                 <button id="kt_login_signin_submit"
                                         class="btn btn-dark font-weight-bolder font-size-h6 px-8 py-4 my-3">Sign In
@@ -132,77 +111,8 @@
     </div>
     <!--end::Login-->
 </div>
-<!--end::Main-->
-{{--<script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>--}}
-<!--begin::Global Config(global config for global JS scripts)-->
-<script>
-    var KTAppSettings = {
-        "breakpoints": {
-            "sm": 576,
-            "md": 768,
-            "lg": 992,
-            "xl": 1200,
-            "xxl": 1400
-        },
-        "colors": {
-            "theme": {
-                "base": {
-                    "white": "#ffffff",
-                    "primary": "#3699FF",
-                    "secondary": "#E5EAEE",
-                    "success": "#1BC5BD",
-                    "info": "#8950FC",
-                    "warning": "#FFA800",
-                    "danger": "#F64E60",
-                    "light": "#E4E6EF",
-                    "dark": "#181C32"
-                },
-                "light": {
-                    "white": "#ffffff",
-                    "primary": "#E1F0FF",
-                    "secondary": "#EBEDF3",
-                    "success": "#C9F7F5",
-                    "info": "#EEE5FF",
-                    "warning": "#FFF4DE",
-                    "danger": "#FFE2E5",
-                    "light": "#F3F6F9",
-                    "dark": "#D6D6E0"
-                },
-                "inverse": {
-                    "white": "#ffffff",
-                    "primary": "#ffffff",
-                    "secondary": "#3F4254",
-                    "success": "#ffffff",
-                    "info": "#ffffff",
-                    "warning": "#ffffff",
-                    "danger": "#ffffff",
-                    "light": "#464E5F",
-                    "dark": "#ffffff"
-                }
-            },
-            "gray": {
-                "gray-100": "#F3F6F9",
-                "gray-200": "#EBEDF3",
-                "gray-300": "#E4E6EF",
-                "gray-400": "#D1D3E0",
-                "gray-500": "#B5B5C3",
-                "gray-600": "#7E8299",
-                "gray-700": "#5E6278",
-                "gray-800": "#3F4254",
-                "gray-900": "#181C32"
-            }
-        },
-        "font-family": "Poppins"
-    };
-</script>
-<!--end::Global Config-->
-<!--begin::Global Theme Bundle(used by all pages)-->
-@include('AdminDashboard.scripts')
-<!--end::Global Theme Bundle-->
-<!--begin::Page Scripts(used by this page)-->
-<script src="{{asset('assets/js/pages/custom/login/login-general.js')}}"></script>
-<!--end::Page Scripts-->
-</body>
-<!--end::Body-->
 
+@include('AdminDashboard.scripts')
+<script src="{{asset('assets/js/pages/custom/login/login-general.js')}}"></script>
+</body>
 </html>
