b07817e88934e08c678e42a69f53fa836e6b497b
Aya Al Rahman <yotayot29@gmail.com>
2022-06-19 11:57:42 +0300

    up

--- resources/views/dashboard/admin/login.blade.php
+++ resources/views/dashboard/admin/login.blade.php
@@ -4,196 +4,204 @@
 
 <head>
     <title>Login to Admin Panel</title>
-    <meta name="description" content="Login page example" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+    <meta name="description" content="Login page example"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"/>
     <!--end::Fonts-->
     <!--begin::Page Custom Styles(used by this page)-->
-    <link href="{{asset('assets/css/pages/login/login-2.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/css/pages/login/login-2.css')}}" rel="stylesheet" type="text/css"/>
     <!--end::Page Custom Styles-->
     <!--begin::Global Theme Styles(used by all pages)-->
-    <link href="{{asset('assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{asset('assets/plugins/custom/prismjs/prismjs.bundle.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{asset('assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/plugins/custom/prismjs/prismjs.bundle.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css"/>
 
     <!--end::Layout Themes-->
-    <link rel="shortcut icon" href="{{asset('assets/media/logos/favicon.ico')}}" />
+    <link rel="shortcut icon" href="{{asset('assets/media/logos/favicon.ico')}}"/>
     {!! htmlScriptTagJsApi([
            'action' => 'homepage',
            'custom_validation' => 'myCustomValidation'
        ]) !!}
+
 </head>
 <!--end::Head-->
 <!--begin::Body-->
 
-<body id="kt_body" class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading">
-    <!--begin::Main-->
-    <div class="d-flex flex-column flex-root">
-        <!--begin::Login-->
-        <div class="login login-2 login-signin-on d-flex flex-column flex-lg-row flex-column-fluid bg-white" id="kt_login">
-            <!--begin::Aside-->
-            <div class="login-aside order-2 order-lg-1 d-flex flex-row-auto position-relative overflow-hidden">
-                <!--begin: Aside Container-->
-                <div class="d-flex flex-column-fluid flex-column justify-content-between py-9 px-7 py-lg-13 px-lg-35">
-                    <!--begin::Logo-->
-                    <a href="{{route('admin.home')}}" class="text-center pt-2">
-                        <img src="{{asset('assets/media/svg/logo.svg')}}" class="max-h-75px" alt="" />
-                    </a>
-                    <!--end::Logo-->
-                    <!--begin::Aside body-->
-                    <div class="d-flex flex-column-fluid flex-column flex-center">
-                        <!--begin::Signin-->
-                        <div class="login-form login-signin py-1">
-                            <!--begin::Form-->
-                            <form id="login-form" class="form" action="{{ route('admin.check') }}" method="post">
-                                @csrf
-                                <!--begin::Title-->
-                                <div class="text-center pb-8">
-                                    <h2 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg">Sign In</h2>
+<body id="kt_body"
+      class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading">
+<!--begin::Main-->
+<div class="d-flex flex-column flex-root">
+    <!--begin::Login-->
+    <div class="login login-2 login-signin-on d-flex flex-column flex-lg-row flex-column-fluid bg-white" id="kt_login">
+        <!--begin::Aside-->
+        <div class="login-aside order-2 order-lg-1 d-flex flex-row-auto position-relative overflow-hidden">
+            <!--begin: Aside Container-->
+            <div class="d-flex flex-column-fluid flex-column justify-content-between py-9 px-7 py-lg-13 px-lg-35">
+                <!--begin::Logo-->
+                <a href="{{route('admin.home')}}" class="text-center pt-2">
+                    <img src="{{asset('assets/media/svg/logo.svg')}}" class="max-h-75px" alt=""/>
+                </a>
+                <!--end::Logo-->
+                <!--begin::Aside body-->
+                <div class="d-flex flex-column-fluid flex-column flex-center">
+                    <!--begin::Signin-->
+                    <div class="login-form login-signin py-1">
+                        <!--begin::Form-->
+                        <form id="login-form" class="form" action="{{ route('admin.check') }}" method="post">
+                        @csrf
+                        <!--begin::Title-->
+                            <div class="text-center pb-8">
+                                <h2 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg">Sign In</h2>
+                            </div>
+                            <!--end::Title-->
+                            <!--begin::Form group-->
+                            <div class="form-group">
+                                <label class="font-size-h6 font-weight-bolder text-dark">Email</label>
+                                <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg" type="text"
+                                       name="email" autocomplete="off"/>
+                                <span class="text-danger">@error('email'){{ $message }}@enderror</span>
+                            </div>
+                            <!--end::Form group-->
+                            <!--begin::Form group-->
+                            <div class="form-group">
+                                <div class="d-flex justify-content-between mt-n5">
+                                    <label class="font-size-h6 font-weight-bolder text-dark pt-5">Password</label>
+                                    <a href="{{route('admin.forget.password.get')}}"
+                                       class="text-primary font-size-h6 font-weight-bolder text-hover-primary pt-5"
+                                       id="kt_login_forgot">Forgot Password ?</a>
                                 </div>
-                                <!--end::Title-->
-                                <!--begin::Form group-->
-                                <div class="form-group">
-                                    <label class="font-size-h6 font-weight-bolder text-dark">Email</label>
-                                    <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg" type="text" name="email" autocomplete="off" />
-                                    <span class="text-danger">@error('email'){{ $message }}@enderror</span>
-                                </div>
-                                <!--end::Form group-->
-                                <!--begin::Form group-->
-                                <div class="form-group">
-                                    <div class="d-flex justify-content-between mt-n5">
-                                        <label class="font-size-h6 font-weight-bolder text-dark pt-5">Password</label>
-                                        <a href="{{route('admin.forget.password.get')}}" class="text-primary font-size-h6 font-weight-bolder text-hover-primary pt-5" id="kt_login_forgot">Forgot Password ?</a>
-                                    </div>
-                                    <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg" type="password" name="password" autocomplete="off" />
-                                    @if(\Illuminate\Support\Facades\Session::has('Error'))
+                                <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg"
+                                       type="password" name="password" autocomplete="off"/>
+                                @if(\Illuminate\Support\Facades\Session::has('Error'))
                                     <span class="text-danger">Current Password Error !</span>
-                                    @endif
-                                </div>
-
-                                <div class="form-group{{ $errors->has('g-recaptcha-response') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Captcha</label>
-
-
-                            <div class="col-md-6">
-                                {!! app('captcha')->display() !!}
-
-
-                                @if ($errors->has('g-recaptcha-response'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
-                                    </span>
                                 @endif
                             </div>
-                        </div>
 
-                                <!--end::Form group-->
-                                <!--begin::Action-->
-                                <div class="text-center pt-2">
-                                    <button id="kt_login_signin_submit" class="btn btn-dark font-weight-bolder font-size-h6 px-8 py-4 my-3">Sign In
-                                    </button>
+                            <div class="form-group{{ $errors->has('g-recaptcha-response') ? ' has-error' : '' }}">
+                                <label class="col-md-4 control-label">Captcha</label>
+
+
+                                <div class="col-md-6">
+                                    {!! app('captcha')->display() !!}
+
+
+                                    @if ($errors->has('g-recaptcha-response'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
+                                    </span>
+                                    @endif
                                 </div>
-                                <!--end::Action-->
-                            </form>
-                            <!--end::Form-->
-                        </div>
-                        <!--end::Signin-->
+                            </div>
 
+                            <!--end::Form group-->
+                            <!--begin::Action-->
+                            <div class="text-center pt-2">
+                                <button id="kt_login_signin_submit"
+                                        class="btn btn-dark font-weight-bolder font-size-h6 px-8 py-4 my-3">Sign In
+                                </button>
+                            </div>
+                            <!--end::Action-->
+                        </form>
+                        <!--end::Form-->
                     </div>
-                    <!--end::Aside body-->
+                    <!--end::Signin-->
+
                 </div>
-                <!--end: Aside Container-->
+                <!--end::Aside body-->
             </div>
-            <!--begin::Aside-->
-            <!--begin::Content-->
-            <div class="content order-1 order-lg-2 d-flex flex-column w-100 pb-0" style="background-color: #B1DCED;">
-                <!--begin::Title-->
-                <div class="d-flex flex-column justify-content-center text-center pt-lg-40 pt-md-5 pt-sm-5 px-lg-0 pt-5 px-7">
-                    <h3 class="display4 font-weight-bolder my-7 text-dark" style="color: #986923;">Amazing Wireframes</h3>
-                    <p class="font-weight-bolder font-size-h2-md font-size-lg text-dark opacity-70">User Experience &amp;
-                        Interface Design, Product Strategy
-                        <br />Web Application SaaS Solutions
-                    </p>
-                </div>
-                <!--end::Title-->
-                <!--begin::Image-->
-                <div class="content-img d-flex flex-row-fluid bgi-no-repeat bgi-position-y-bottom bgi-position-x-center" style="background-image: url(assets/media/svg/illustrations/login-visual-2.svg);"></div>
-                <!--end::Image-->
-            </div>
-            <!--end::Content-->
+            <!--end: Aside Container-->
         </div>
-        <!--end::Login-->
+        <!--begin::Aside-->
+        <!--begin::Content-->
+        <div class="content order-1 order-lg-2 d-flex flex-column w-100 pb-0" style="background-color: #B1DCED;">
+            <!--begin::Title-->
+            <div class="d-flex flex-column justify-content-center text-center pt-lg-40 pt-md-5 pt-sm-5 px-lg-0 pt-5 px-7">
+                <h3 class="display4 font-weight-bolder my-7 text-dark" style="color: #986923;">Amazing Wireframes</h3>
+                <p class="font-weight-bolder font-size-h2-md font-size-lg text-dark opacity-70">User Experience &amp;
+                    Interface Design, Product Strategy
+                    <br/>Web Application SaaS Solutions
+                </p>
+            </div>
+            <!--end::Title-->
+            <!--begin::Image-->
+            <div class="content-img d-flex flex-row-fluid bgi-no-repeat bgi-position-y-bottom bgi-position-x-center"
+                 style="background-image: url(assets/media/svg/illustrations/login-visual-2.svg);"></div>
+            <!--end::Image-->
+        </div>
+        <!--end::Content-->
     </div>
-    <!--end::Main-->
-    {{--<script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>--}}
-    <!--begin::Global Config(global config for global JS scripts)-->
-    <script>
-        var KTAppSettings = {
-            "breakpoints": {
-                "sm": 576,
-                "md": 768,
-                "lg": 992,
-                "xl": 1200,
-                "xxl": 1400
-            },
-            "colors": {
-                "theme": {
-                    "base": {
-                        "white": "#ffffff",
-                        "primary": "#3699FF",
-                        "secondary": "#E5EAEE",
-                        "success": "#1BC5BD",
-                        "info": "#8950FC",
-                        "warning": "#FFA800",
-                        "danger": "#F64E60",
-                        "light": "#E4E6EF",
-                        "dark": "#181C32"
-                    },
-                    "light": {
-                        "white": "#ffffff",
-                        "primary": "#E1F0FF",
-                        "secondary": "#EBEDF3",
-                        "success": "#C9F7F5",
-                        "info": "#EEE5FF",
-                        "warning": "#FFF4DE",
-                        "danger": "#FFE2E5",
-                        "light": "#F3F6F9",
-                        "dark": "#D6D6E0"
-                    },
-                    "inverse": {
-                        "white": "#ffffff",
-                        "primary": "#ffffff",
-                        "secondary": "#3F4254",
-                        "success": "#ffffff",
-                        "info": "#ffffff",
-                        "warning": "#ffffff",
-                        "danger": "#ffffff",
-                        "light": "#464E5F",
-                        "dark": "#ffffff"
-                    }
+    <!--end::Login-->
+</div>
+<!--end::Main-->
+{{--<script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>--}}
+<!--begin::Global Config(global config for global JS scripts)-->
+<script>
+    var KTAppSettings = {
+        "breakpoints": {
+            "sm": 576,
+            "md": 768,
+            "lg": 992,
+            "xl": 1200,
+            "xxl": 1400
+        },
+        "colors": {
+            "theme": {
+                "base": {
+                    "white": "#ffffff",
+                    "primary": "#3699FF",
+                    "secondary": "#E5EAEE",
+                    "success": "#1BC5BD",
+                    "info": "#8950FC",
+                    "warning": "#FFA800",
+                    "danger": "#F64E60",
+                    "light": "#E4E6EF",
+                    "dark": "#181C32"
                 },
-                "gray": {
-                    "gray-100": "#F3F6F9",
-                    "gray-200": "#EBEDF3",
-                    "gray-300": "#E4E6EF",
-                    "gray-400": "#D1D3E0",
-                    "gray-500": "#B5B5C3",
-                    "gray-600": "#7E8299",
-                    "gray-700": "#5E6278",
-                    "gray-800": "#3F4254",
-                    "gray-900": "#181C32"
+                "light": {
+                    "white": "#ffffff",
+                    "primary": "#E1F0FF",
+                    "secondary": "#EBEDF3",
+                    "success": "#C9F7F5",
+                    "info": "#EEE5FF",
+                    "warning": "#FFF4DE",
+                    "danger": "#FFE2E5",
+                    "light": "#F3F6F9",
+                    "dark": "#D6D6E0"
+                },
+                "inverse": {
+                    "white": "#ffffff",
+                    "primary": "#ffffff",
+                    "secondary": "#3F4254",
+                    "success": "#ffffff",
+                    "info": "#ffffff",
+                    "warning": "#ffffff",
+                    "danger": "#ffffff",
+                    "light": "#464E5F",
+                    "dark": "#ffffff"
                 }
             },
-            "font-family": "Poppins"
-        };
-    </script>
-    <!--end::Global Config-->
-    <!--begin::Global Theme Bundle(used by all pages)-->
-    @include('AdminDashboard.scripts')
-    <!--end::Global Theme Bundle-->
-    <!--begin::Page Scripts(used by this page)-->
-    <script src="{{asset('assets/js/pages/custom/login/login-general.js')}}"></script>
-    <!--end::Page Scripts-->
+            "gray": {
+                "gray-100": "#F3F6F9",
+                "gray-200": "#EBEDF3",
+                "gray-300": "#E4E6EF",
+                "gray-400": "#D1D3E0",
+                "gray-500": "#B5B5C3",
+                "gray-600": "#7E8299",
+                "gray-700": "#5E6278",
+                "gray-800": "#3F4254",
+                "gray-900": "#181C32"
+            }
+        },
+        "font-family": "Poppins"
+    };
+</script>
+<!--end::Global Config-->
+<!--begin::Global Theme Bundle(used by all pages)-->
+@include('AdminDashboard.scripts')
+<!--end::Global Theme Bundle-->
+<!--begin::Page Scripts(used by this page)-->
+<script src="{{asset('assets/js/pages/custom/login/login-general.js')}}"></script>
+<!--end::Page Scripts-->
 </body>
 <!--end::Body-->
 
