2f782537ee7d7bf54d0bbd6623cc1075c93161f2
Aya Al Rahman <yotayot29@gmail.com>
2022-06-19 14:36:47 +0300

    recaptcha

--- resources/views/dashboard/admin/login.blade.php
+++ resources/views/dashboard/admin/login.blade.php
@@ -5,7 +5,6 @@
 <head>
     <title>Login to Admin Panel</title>
     <meta name="description" content="Login page example"/>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"/>
     <!--end::Fonts-->
@@ -16,11 +15,13 @@
     <link href="{{asset('assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css"/>
     <link href="{{asset('assets/plugins/custom/prismjs/prismjs.bundle.css')}}" rel="stylesheet" type="text/css"/>
     <link href="{{asset('assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css"/>
-
+    <script src='https://www.google.com/recaptcha/api.js'></script>
     <!--end::Layout Themes-->
     <link rel="shortcut icon" href="{{asset('assets/media/logos/favicon.ico')}}"/>
-    <script src='https://www.google.com/recaptcha/api.js'></script>s
-
+{{--    {!! htmlScriptTagJsApi([--}}
+{{--           'action' => 'homepage',--}}
+{{--           'custom_validation' => 'myCustomValidation'--}}
+{{--       ]) !!}--}}
 
 </head>
 <!--end::Head-->
@@ -78,10 +79,9 @@
 
                             <div class="form-group{{ $errors->has('g-recaptcha-response') ? ' has-error' : '' }}">
                                 <label class="col-md-4 control-label">Captcha</label>
+                                <div class="g-recaptcha" data-sitekey="{!! env('RECAPTCHA_SITE_KEY') !!}"></div>
 
-                                <div class="g-recaptcha" data-sitekey="6LdjwHogAAAAAId77VK90hzQOjHJZJF5LpHTXJI2"></div>
-
-{{--                                <div class="col-md-6">--}}
+                                {{--                                <div class="col-md-6">--}}
 {{--                                    {!! app('captcha')->display() !!}--}}
 
 
