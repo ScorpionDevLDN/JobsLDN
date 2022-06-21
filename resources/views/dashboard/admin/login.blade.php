c7f3aea34281a56f8491abb96f54cf946cae9504
Aya Al Rahman <yotayot29@gmail.com>
2022-06-21 14:18:58 +0300

    up

--- resources/views/dashboard/admin/login.blade.php
+++ resources/views/dashboard/admin/login.blade.php
@@ -6,44 +6,43 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
-
 <body>
-<div class="container">
-    <div class="row m-5 no-gutters shadow-lg">
-        <div class="col-md-6 d-none d-md-block">
-            <img src="https://images.unsplash.com/photo-1566888596782-c7f41cc184c5?ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80" class="img-fluid" style="min-height:100%;" />
-        </div>
-        <div class="col-md-6 bg-white p-5">
-            <h3 class="pb-3">Login Form</h3>
-            <div class="form-style">
-                <form>
-                    <div class="form-group pb-3">
-                        <input type="email" placeholder="Email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    </div>
-                    <div class="form-group pb-3">
-                        <input type="password" placeholder="Password" class="form-control" id="exampleInputPassword1">
-                    </div>
-                    <div class="d-flex align-items-center justify-content-between">
-                        <div class="d-flex align-items-center"><input name="" type="checkbox" value="" /> <span class="pl-2 font-weight-bold">Remember Me</span></div>
-                        <div><a href="#">Forget Password?</a></div>
-                    </div>
-                    <div class="pb-2">
-                        <button type="submit" class="btn btn-dark w-100 font-weight-bold mt-2">Submit</button>
-                    </div>
-                </form>
-                <div class="sideline">OR</div>
-                <div>
-                    <button type="submit" class="btn btn-primary w-100 font-weight-bold mt-2"><i class="fa fa-facebook" aria-hidden="true"></i> Login With Facebook</button>
-                </div>
-                <div class="pt-4 text-center">
-                    Get Members Benefit. <a href="#">Sign Up</a>
+
+<!------ Include the above in your HEAD tag ---------->
+
+<div id="login">
+    <h3 class="text-center text-white pt-5">Login form</h3>
+    <div class="container">
+        <div id="login-row" class="row justify-content-center align-items-center">
+            <div id="login-column" class="col-md-6">
+                <div id="login-box" class="col-md-12">
+                    <form id="login-form" class="form" action="" method="post">
+                        <h3 class="text-center text-info">Login</h3>
+                        <div class="form-group">
+                            <label for="username" class="text-info">Username:</label><br>
+                            <input type="text" name="username" id="username" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="text-info">Password:</label><br>
+                            <input type="text" name="password" id="password" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
+                            <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
+                        </div>
+                        <div id="register-link" class="text-right">
+                            <a href="#" class="text-info">Register here</a>
+                        </div>
+                    </form>
                 </div>
             </div>
-
         </div>
     </div>
 </div>
-<script src="https://use.fontawesome.com/f59bcd8580.js"></script>
+
 </body>
 </html>
