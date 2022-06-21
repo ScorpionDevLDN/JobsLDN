f4523f80fbbd9eca9721255827c618f88def3083
Aya Al Rahman <yotayot29@gmail.com>
2022-06-21 14:30:55 +0300

    up

--- resources/views/dashboard/admin/login.blade.php
+++ resources/views/dashboard/admin/login.blade.php
@@ -11,7 +11,83 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
+<styl>
+    body {
+    font-family: "Lato", sans-serif;
+    }
 
+
+
+    .main-head{
+    height: 150px;
+    background: #FFF;
+
+    }
+
+    .sidenav {
+    height: 100%;
+    background-color: #000;
+    overflow-x: hidden;
+    padding-top: 20px;
+    }
+
+
+    .main {
+    padding: 0px 10px;
+    }
+
+    @media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+    }
+
+    @media screen and (max-width: 450px) {
+    .login-form{
+    margin-top: 10%;
+    }
+
+    .register-form{
+    margin-top: 10%;
+    }
+    }
+
+    @media screen and (min-width: 768px){
+    .main{
+    margin-left: 40%;
+    }
+
+    .sidenav{
+    width: 40%;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    }
+
+    .login-form{
+    margin-top: 80%;
+    }
+
+    .register-form{
+    margin-top: 20%;
+    }
+    }
+
+
+    .login-main-text{
+    margin-top: 20%;
+    padding: 60px;
+    color: #fff;
+    }
+
+    .login-main-text h2{
+    font-weight: 300;
+    }
+
+    .btn-black{
+    background-color: #000 !important;
+    color: #fff;
+    }
+</styl>
 <!------ Include the above in your HEAD tag ---------->
 
 <div id="login">
@@ -20,11 +96,12 @@
         <div id="login-row" class="row justify-content-center align-items-center">
             <div id="login-column" class="col-md-6">
                 <div id="login-box" class="col-md-12">
-                    <form id="login-form" class="form" action="" method="post">
+                    <form id="login-form" class="form" action="{{ route('admin.check') }}" method="post">
+                        @csrf
                         <h3 class="text-center text-info">Login</h3>
                         <div class="form-group">
-                            <label for="username" class="text-info">Username:</label><br>
-                            <input type="text" name="username" id="username" class="form-control">
+                            <label for="username" class="text-info">Email:</label><br>
+                            <input type="text" name="username" id="email" class="form-control">
                         </div>
                         <div class="form-group">
                             <label for="password" class="text-info">Password:</label><br>
