77c2e4264c65e1de4db180114cb47c08635cb000
Aya Al Rahman <yotayot29@gmail.com>
2022-06-22 09:46:38 +0300

    forget

--- resources/views/auth/Sent.blade.php
+++ resources/views/auth/Sent.blade.php
@@ -1,18 +1,31 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<!--begin::Head-->
+<head>
+    <title>Email Sent</title>
+    <meta name="description" content="Login page example"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"/>
+    <!--end::Fonts-->
+    <!--begin::Page Custom Styles(used by this page)-->
+    <link href="{{asset('assets/css/pages/login/login-2.css')}}" rel="stylesheet" type="text/css"/>
+    <!--end::Page Custom Styles-->
+    <!--begin::Global Theme Styles(used by all pages)-->
+    <link href="{{asset('assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/plugins/custom/prismjs/prismjs.bundle.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css"/>
 
-@section('content')
-    <main class="login-form">
-        <div class="cotainer">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">Email Sent</div>
-                        <div class="card-body">
-                            <h5>Email Sent Successfully</h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main>
-@endsection
+    <!--end::Layout Themes-->
+    <link rel="shortcut icon" href="{{asset('assets/media/logos/favicon.ico')}}"/>
+</head>
+<!--end::Head-->
+<!--begin::Body-->
+<body id="kt_body"
+      class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading">
+<h5>Password link sent to your email successfully</h5>
+
+@include('AdminDashboard.scripts')
+
+</body>
+<!--end::Body-->
+</html>
