56b4f921ca7c095b261041c703b8ebb8a456e45b
Aya Al Rahman <yotayot29@gmail.com>
2022-07-03 10:41:07 +0300

    paypal_settings

--- resources/views/dashboard/admin/payments/index.blade.php
+++ resources/views/dashboard/admin/payments/index.blade.php
@@ -3,83 +3,157 @@
     <a class="btn">Jobs</a>
     <a class="btn">Payment</a>
 @endsection
-@section('title','Payment')
+@section('title' ,'Settings')
+@section('js')
+    <script type="text/javascript">
+        $('.settings-tab-opener').on('click', function () {
+            $('.settings-tab-opener').removeClass('active');
+            $(this).addClass('active');
+            var open_id = $(this).attr('data-opentab');
+            $('.taber').removeClass('active');
+            $('.taber#' + open_id).addClass('active');
+        });
+    </script>
+@endsection
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 col-sm-12">
-                <div class="card card-custom gutter-b example example-compact">
-                    <div class="card-header">
-                        <h3 class="card-title">Payment Settings</h3>
-                    </div>
-                    <!--begin::Form-->
-                    <form enctype="multipart/form-data" action="{{route('admin.get-payments.update',1)}}" method="post">
-                        @csrf
-                        @method('put')
-                        <div class="card-body">
-                            <div class="form-group row">
-                                <label class="col-3 col-form-label">Title</label>
-                                <div class="col-9">
-                                    <input value="{{$payment->title}}"  name="title" class="form-control" type="text"  id="example-text-input">
-                                    <span class="text-danger">@error('title'){{ $message }}@enderror</span>
+    <div class="row">
+        <div class="col-md-12 col-sm-12">
+            <div class="card card-custom">
+                <!--begin::Header-->
+                <div class="card-header card-header-tabs-line">
+                    <ul class="nav nav-dark nav-bold nav-tabs nav-tabs-line" data-remember-tab="tab_id" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-toggle="tab" href="#kt_builder_themes">General</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#kt_builder_page">Setting</a>
+                        </li>
+                    </ul>
+                </div>
+                <!--end::Header-->
+                <!--begin::Form-->
+                <form enctype="multipart/form-data" action="{{route('admin.get-payments.update',1)}}" method="post">
+                @csrf
+                @method('put')
+                @csrf
+                <!--begin::Body-->
+                    <div class="card-body">
+                        <div class="tab-content pt-3">
+                            <!--begin::Tab Pane-->
+                            <div class="tab-pane active" id="kt_builder_themes">
+                                <div class="form-group row">
+                                    <label class="col-3 col-form-label">Title</label>
+                                    <div class="col-9">
+                                        <input value="{{$payment->title}}" name="title" class="form-control" type="text"
+                                               id="example-text-input">
+                                        <span class="text-danger">@error('title'){{ $message }}@enderror</span>
 
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div class="form-group row">
-                                <label class="col-3 col-form-label">Image</label>
-                                <div class="form-group col-9">
-                                    <div class="custom-file">
-                                        <input name="image" type="file" class="custom-file-input" id="customFile">
-                                        <label class="custom-file-label" for="customFile">Choose file</label>
-                                        <img src="{{$payment->image}}" alt="">
-                                        <span class="text-danger">@error('image'){{ $message }}@enderror</span>
+                                <div class="form-group row">
+                                    <label class="col-3 col-form-label">Image</label>
+                                    <div class="form-group col-9">
+                                        <div class="custom-file">
+                                            <input name="image" type="file" class="custom-file-input" id="customFile">
+                                            <label class="custom-file-label" for="customFile">Choose file</label>
+                                            <img src="{{$payment->image}}" alt="">
+                                            <span class="text-danger">@error('image'){{ $message }}@enderror</span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-3 col-form-label">Description</label>
+                                    <div class="col-9">
+                                    <textarea rows="8" name="description" class="form-control" type="text"
+                                              id="example-text-input">
+                                        {{$payment->description}}
+                                    </textarea>
+                                        <span class="text-danger">@error('description'){{ $message }}@enderror</span>
+
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-3 col-form-label">Days Count</label>
+                                    <div class="col-9">
+                                        <input value="{{$payment->days_count}}" name="days_count" class="form-control"
+                                               type="number" id="example-text-input">
+                                        <span class="text-danger">@error('days_count'){{ $message }}@enderror</span>
+
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-3 col-form-label">Text</label>
+                                    <div class="col-9">
+                                        <input value="{{$payment->text}}" name="text" class="form-control" type="text"
+                                               id="example-text-input">
+                                        <span class="text-danger">@error('text'){{ $message }}@enderror</span>
+
                                     </div>
                                 </div>
                             </div>
+                            <!--end::Tab Pane-->
+                            <!--begin::Tab Pane-->
+                            <div class="tab-pane" id="kt_builder_page">
+                                <div class="col-12 px-10 d-flex mb-3 row pb-3">
+                                    <span>
+                                        Get your keys from here:
+                                        <a target="_blank" href="https://developer.paypal.com/developer/applications">
+                                            Paypal Developer
+                                        </a>
+                                    </span>
 
-                            <div class="form-group row">
-                                <label class="col-3 col-form-label">Description</label>
-                                <div class="col-9">
-                                    <textarea rows="8"  name="description" class="form-control" type="text"  id="example-text-input">
-                                        {{$payment->description}}
-                                    </textarea>
-                                    <span class="text-danger">@error('description'){{ $message }}@enderror</span>
+                                </div>
+                                <div class="col-12 px-10 d-flex mb-3 row pb-3">
+                                    <div class="col-12 col-lg-3 px-2 text-lg-end pt-1 pb-3 pb-lg-0">
+                                        Client Id
+                                    </div>
 
+                                    <div class="col-12 col-lg-9 px-2">
+                                        <input name="client_id" class="form-control" type="text"
+                                               value=""
+                                               id="example-color-input">
+                                    </div>
+                                </div>
+
+                                <div class="col-12 px-10 d-flex mb-3 row pb-3">
+                                    <div class="col-12 col-lg-3 px-2 text-lg-end pt-1 pb-3 pb-lg-0">
+                                        Secret Id
+                                    </div>
+
+                                    <div class="col-12 col-lg-9 px-2">
+                                        <input name="secret_id" class="form-control" type="text"
+                                               value=""
+                                               id="example-color-input">
+                                    </div>
                                 </div>
                             </div>
+                            <!--end::Tab Pane-->
 
-                            <div class="form-group row">
-                                <label class="col-3 col-form-label">Days Count</label>
-                                <div class="col-9">
-                                    <input value="{{$payment->days_count}}"  name="days_count" class="form-control" type="number"  id="example-text-input">
-                                    <span class="text-danger">@error('days_count'){{ $message }}@enderror</span>
-
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label class="col-3 col-form-label">Text</label>
-                                <div class="col-9">
-                                    <input value="{{$payment->text}}"  name="text" class="form-control" type="text"  id="example-text-input">
-                                    <span class="text-danger">@error('text'){{ $message }}@enderror</span>
-
-                                </div>
-                            </div>
-
-
-                        <div class="card-footer">
-                            <div class="row">
-                                <div class="col-lg-3"></div>
-                                <div class="col-lg-9">
-                                    <button type="submit" class="btn btn-primary mr-2">Submit</button>
-                                    <a href="{{route('admin.pages.index')}}" type="button" class="button btn btn-secondary">Cancel</a>
-                                </div>
+                        </div>
+                    </div>
+                    <!--end::Body-->
+                    <!--begin::Footer-->
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-lg-9">
+                                <input type="hidden" id="tab_id" name="builder[tab][tab_id]">
+                                <input type="hidden" id="tab_extra_id" name="builder[tab][tab_extra_id]">
+                                <button type="submit" name="builder_submit" data-demo="demo1"
+                                        class="btn btn-primary font-weight-bold mr-2 px-5">Submit
+                                </button>
+                                <a href="{{route('admin.home')}}" data-demo="demo1"
+                                   class="button btn btn-clean font-weight-bold">Cancel</a>
                             </div>
                         </div>
-                    </form>
-                    <!--end::Form-->
-                </div>
+                    </div>
+                    <!--end::Footer-->
+                </form>
+                <!--end::Form-->
+
             </div>
         </div>
     </div>
