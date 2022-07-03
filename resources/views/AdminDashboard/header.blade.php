f69f1bde94f858005e2c321c23e251e9b6fc3502
Aya Al Rahman <yotayot29@gmail.com>
2022-07-03 22:57:04 +0300

    up

--- resources/views/AdminDashboard/header.blade.php
+++ resources/views/AdminDashboard/header.blade.php
@@ -2,7 +2,7 @@
     <base href="../../">
     <meta charset="utf-8" />
     <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1"/>
-    <title>JOBSLDN| @yield('title')</title>
+    <title>JOBSLDN @yield('title')</title>
     <meta name="description" content="Metronic admin dashboard live demo. Check out all the features of the admin panel. A large number of settings, additional services and widgets." />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="canonical" href="https://keenthemes.com/metronic" />
