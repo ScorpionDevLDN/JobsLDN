2c0fc644837f0a6fe3f116bc827edec3bcb31b71
aya_al_rahman <ayakomar@gmail.com>
2022-09-13 13:35:25 +0300

    job_seeker

--- resources/views/frontend/jobsldn/app/parts/job_seeker_horizental.blade.php
+++ resources/views/frontend/jobsldn/app/parts/job_seeker_horizental.blade.php
@@ -17,3 +17,25 @@
         </ul>
     </div>
 </div>
+
+<div class="modal fade" id="logout" tabindex="-1" aria-labelledby="logoutLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6 class="modal-title" id="logoutLabel">Notification</h6>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                    <ion-icon name="close-outline"></ion-icon>
+                </button>
+            </div>
+            <form action="{{route('logoutFront')}}" method="post">
+                @csrf
+                <div class="modal-body">
+                    <h5 class="text-center">Are you sure you want to log out?</h5>
+                </div>
+                <div class="modal-footer">
+                    <button class="button mx-auto" type="submit">Yes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
