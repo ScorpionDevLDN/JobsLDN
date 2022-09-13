6a74cfd2c89acf74ff8ae09cba7be3ae25ab4413
aya_al_rahman <ayakomar@gmail.com>
2022-09-13 15:47:40 +0300

    up

--- resources/views/FrontLayout/footer_script.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- Scripts-->
-<script src="{{asset('assets/dist/bundled.js')}}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-
-
-@yield('js')
